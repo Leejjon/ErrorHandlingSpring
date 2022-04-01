7c6155cde2fccc6777b75e6e56f592ff7a0301c4
Leejjon <leejjon@gmail.com>
2022-04-01 18:00:15 +0200

    Add log statement.

--- src/main/java/net/leejjon/demo/GetSomethingController.java
+++ src/main/java/net/leejjon/demo/GetSomethingController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 public class GetSomethingController {
@@ -26,8 +28,8 @@ public class GetSomethingController {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleUnexpectedErrors(Exception e) {
-        // You should log the exception here!
+    public String handleUnexpectedErrors(HttpServletRequest req, Exception e) {
+        log.error("Unexpected error occurred on request: " + req.getServletPath(), e);
         return "Error";
     }
 }
