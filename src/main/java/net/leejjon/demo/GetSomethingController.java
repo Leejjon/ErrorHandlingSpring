328ddc5c7ab3a50021e63ed1edd41f6220a5ac33
Leejjon <leejjon@gmail.com>
2022-04-01 17:47:58 +0200

    Adding logger

--- src/main/java/net/leejjon/demo/GetSomethingController.java
+++ src/main/java/net/leejjon/demo/GetSomethingController.java
@@ -1,5 +1,6 @@
 package net.leejjon.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class GetSomethingController {
     private final BusinessLogic businessLogic;
