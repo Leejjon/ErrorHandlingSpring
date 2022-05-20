9f616d1f795a40aead2090770a50d84e3a2c39aa
Leejjon <leejjon@gmail.com>
2022-05-20 15:53:45 +0200

    Made the database work.

--- src/main/java/net/leejjon/demo/DatabaseService.java
+++ src/main/java/net/leejjon/demo/DatabaseService.java
@@ -2,25 +2,32 @@ package net.leejjon.demo;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.UUID;
+import java.util.List;
 
 @Service
 @Slf4j
 public class DatabaseService {
-    @SneakyThrows
-    public void runQuery(boolean extraParam) {
-        try {
-            if (extraParam) {
-                throw new SQLException();
+    private JdbcTemplate jdbcTemplate;
+
+    public DatabaseService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void runQuery(boolean param) {
+        String sql = "select * from records where param = ?";
+        List<String> ids = jdbcTemplate.query(sql, new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return rs.getString("id");
             }
-        } catch (SQLException e) {
-            final String uuid = UUID.randomUUID().toString();
-            log.error(uuid + " Got error " + e.getMessage() +
-                    " on query select * from data where extraParam = " + extraParam, e);
-            throw new AlreadyLoggedException(e, uuid);
-        }
+        }, param);
+
+        log.info("We retrieved " + ids.size() + " records.");
     }
 }
