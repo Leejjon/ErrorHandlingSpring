4ccd2cc6d4d0dabafa50af06ec0ed978b43562bc
Leejjon <leejjon@gmail.com>
2022-05-20 16:15:14 +0200

    Rethrow example again.

--- src/main/java/net/leejjon/demo/DatabaseService.java
+++ src/main/java/net/leejjon/demo/DatabaseService.java
@@ -2,12 +2,12 @@ package net.leejjon.demo;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -21,13 +21,21 @@ public class DatabaseService {
 
     public void runQuery(boolean param) {
         String sql = "select * from records where param = ?";
-        List<String> ids = jdbcTemplate.query(sql, new RowMapper<String>() {
-            @Override
-            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return rs.getString("id");
-            }
-        }, param);
-
-        log.info("We retrieved " + ids.size() + " records.");
+        try {
+            List<String> ids = jdbcTemplate.query(sql,
+                    new RowMapper<String>() {
+                        @Override
+                        @SneakyThrows
+                        public String mapRow(ResultSet rs, int rowNum) {
+                            return rs.getString("id");
+                        }
+                    }, param);
+            log.info("We retrieved " + ids.size() + " records.");
+        } catch (DataAccessException e) {
+            log.error("Failed running query: " + sql);
+            log.error("Parameters: param=" + param);
+            log.error("Exception: ", e);
+            throw e;
+        }
     }
 }
