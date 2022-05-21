a575697af66e2e0b353de18d44c88ff5ea487e48
Leejjon <leejjon@gmail.com>
2022-05-21 22:40:57 +0200

    Save the changes

--- src/main/java/net/leejjon/demo/AlreadyLoggedException.java
+++
@@ -1,7 +0,0 @@
-package net.leejjon.demo;
-
-public class AlreadyLoggedException extends RuntimeException {
-    public AlreadyLoggedException(Exception e) {
-        super(e);
-    }
-}
--- src/main/java/net/leejjon/demo/Main.java
+++ src/main/java/net/leejjon/demo/Main.java
@@ -1,26 +1,60 @@
 package net.leejjon.demo;
 
 import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            justAMethodInBetween();
-        } catch (Exception e) {
-            e.printStackTrace();
+        BusinessLogic businessLogic = new BusinessLogic(new DatabaseService());
+        Controller controller = new Controller(businessLogic);
+        controller.callApi();
+    }
+
+    static class Controller {
+        private final BusinessLogic businessLogic;
+
+        public Controller(BusinessLogic businessLogic) {
+            this.businessLogic = businessLogic;
+        }
+
+        public String callApi() {
+            try {
+                businessLogic.doBusinessLogic();
+                return "200";
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "500";
+            }
         }
     }
 
-    private static void justAMethodInBetween() {
-        checkedException();
+    static class BusinessLogic {
+        private final DatabaseService databaseService;
+
+        public BusinessLogic(DatabaseService databaseService) {
+            this.databaseService = databaseService;
+        }
+
+        public void doBusinessLogic() {
+            databaseService.runQuery(true);
+        }
     }
 
-    @SneakyThrows
-    private static void checkedException() {
-        if (new Random().nextBoolean()) {
-            throw new SQLException();
+    static class DatabaseService {
+        @SneakyThrows
+        public void runQuery(boolean extraParam) {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:hsqldb:mem:testdb;DB_CLOSE_DELAY=-1"
+            ); // Some simple in memory db
+            String sql = "select * from records where extraParam = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, Boolean.toString(extraParam));
+            ps.executeQuery();
+            con.close();
         }
     }
 }
