bb1dd47bbbc3d79b51f0ee82cd32eec65fb6b7f8
Leejjon <leejjon@gmail.com>
2022-05-31 10:45:02 +0200

    Change

--- src/main/java/net/leejjon/demo/GetSomethingController.java
+++ src/main/java/net/leejjon/demo/GetSomethingController.java
@@ -4,14 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Slf4j
+@Validated
 @RestController
 public class GetSomethingController {
     private final BusinessLogic businessLogic;
@@ -22,9 +27,9 @@ public class GetSomethingController {
     }
 
     @GetMapping("/")
-    public String getSomething() {
+    public String getSomething(@RequestParam @Valid @Size(min = 3, max = 10) String name) {
         businessLogic.doBusinessLogic();
-        return "Hello";
+        return "Hello: " + name;
     }
 
     @PostMapping("/post")
@@ -33,9 +38,22 @@ public class GetSomethingController {
         return "Posted";
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleValidationError() {
+        return "Bad request";
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMissingRequestParameterError() {
+        return "Bad request";
+    }
+
     @ExceptionHandler(AlreadyLoggedException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleUnexpectedErrorsThatAreAlreadyLogged(
+
             AlreadyLoggedException e) {
         // Do not log
         return "Error: " + e.getUuid();
@@ -51,7 +69,7 @@ public class GetSomethingController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationErrors(HttpServletRequest req, MethodArgumentNotValidException e) {
+    public String handleValidationErrors(MethodArgumentNotValidException e) {
         StringBuilder validationErrorMessage = new StringBuilder("Validation error: \n");
         for (ObjectError vallidationError : e.getAllErrors()) {
             validationErrorMessage.append(vallidationError.getDefaultMessage());
